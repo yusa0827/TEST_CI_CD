b942d673f95d5139511fabfbd849a70085908ecb
yusa0827 <sasaki.yudai.2016@gmail.com>
2024-08-25 16:51:40 +0900

    add test function

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def hello_world():
     return "Hello, CI/CD!"
 
+def hello_world_2():
+    return "Hello, CI/CD! 2"
+
 if __name__ == "__main__":
     print(hello_world())
+    print(hello_world_2())
